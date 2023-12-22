4af6ed26dd8c5f0f43bda2efa8375d85f08cbc7c
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2023-12-22 10:38:14 +0530

    models creation

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit:"16kb"})); //body parser
+app.use(express.urlencoded({extended:true,limit:"16kb"})); // url parser
+app.use(express.static("public"));
+
+
+// parse cookies
+app.use(cookieParser());
+
+
+export { app }
